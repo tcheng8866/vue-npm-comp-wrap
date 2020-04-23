b1cbdf81efbbb09369dbbb268fcac48d8ba8968d
“tianchengcheng” <tian.chengcheng@vpclub.cn>
2020-04-23 18:00:56 +0800

    变更仓库地址

---
+++ README.md
@@ -0,0 +1,5 @@
+# "comp-wrap"简介
+封装常用的前端组件
+## 功能列表
+- w-input  label/input 组件 
+
--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-import PInput from './components/p-input/index.vue'
-import PPicker from './components/p-picker/index.vue'
-import PTab from './components/p-tab/index.vue'
+import wInput from './components/p-input/index.vue'
 
 export {
-	PInput,
-	PPicker,
-	PTab
+  wInput
 }
