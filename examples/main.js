3a0e65e1d7fbf2f087a49362fe524e21343a142d
“tianchengcheng” <tian.chengcheng@vpclub.cn>
2020-04-26 10:40:04 +0800

    优化

--- examples/main.js
+++ examples/main.js
@@ -1,15 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-/* // 导入组件库
 import compWrap from './../packages/index'
-// 注册组件库
-Vue.use(compWrap) */
-
-import wInput from './../packages/index'
-Vue.use(wInput)
-import wButton from './../packages/index'
-Vue.use(wButton)
+Vue.use(compWrap)
 
 Vue.config.productionTip = false
 new Vue({
